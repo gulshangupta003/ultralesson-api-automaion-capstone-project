b4c0e0a661ba1351ffc5e2fafea36bfacab26215
gulshangupta003 <gulshangupta003@gmail.com>
2023-12-11 17:56:13 +0530

    Added random email generator code

---
+++ src/main/java/RandomEmailGenerator.java
@@ -0,0 +1,7 @@
+import java.util.UUID;
+
+public class RandomEmailGenerator {
+    public static String generateRandomEmail() {
+        return UUID.randomUUID().toString() + "@gmail.com";
+    }
+}
--- src/test/java/UserSignupTest.java
+++ src/test/java/UserSignupTest.java
@@ -1,23 +1,27 @@
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
-import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
 
 public class UserSignupTest extends BaseTest {
     @Test
     public void successfullySignupUser() {
-        String requestBody = "{\n" +
-                "    \"email\": \"user1234567@example.com\",\n" +
-                "    \"password\": \"password123\"\n" +
-                "}";
+        String randomEmail = RandomEmailGenerator.generateRandomEmail();
+        String password = "password123";
+
+        String requestBody = String.format("{\n" +
+                "    \"email\": \"%s\",\n" +
+                "    \"password\": \"%s\"\n" +
+                "}", randomEmail, password);
 
         Response response = RestAssured.given()
                 .contentType(ContentType.JSON)
                 .body(requestBody)
                 .post("/api/auth/signup");
 
-        Assert.assertEquals(response.getStatusCode(), 201);
-        Assert.assertEquals(response.jsonPath().getString("data.user.email"), "user1234567@example.com");
+        assertEquals(response.getStatusCode(), 201);
+        assertEquals(response.jsonPath().getString("data.user.email"), randomEmail);
     }
 }
