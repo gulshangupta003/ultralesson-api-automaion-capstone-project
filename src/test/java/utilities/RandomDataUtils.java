4a8be815c2e56465ef4ab39d4f46bd55e62a4e8b
gulshangupta003 <gulshangupta003@gmail.com>
2023-12-13 18:51:33 +0530

    Added documentation in RandomDataUtils class

--- src/test/java/utilities/RandomDataUtils.java
+++ src/test/java/utilities/RandomDataUtils.java
@@ -2,13 +2,28 @@ package utilities;
 
 import com.github.javafaker.Faker;
 
+/**
+ * A utility class for generating random email addresses using the Faker library.
+ */
 public class RandomDataUtils {
     private Faker faker = new Faker();
 
+
+    /**
+     * Generates a completely random email address.
+     *
+     * @return A randomly generated email address.
+     */
     public String generateRandomEmail() {
         return faker.internet().emailAddress();
     }
 
+    /**
+     * Generates a random email address with a random username and the specified domain.
+     *
+     * @param domain domain The domain for the email address.
+     * @return A randomly generated email address.
+     */
     public String generateRandomEmail(String domain) {
         return faker.name().username() + "@" + domain;
     }
