ed1bfb32ee3868c1e9bdf448dd531bd7b6f046b3
gulshangupta003 <gulshangupta003@gmail.com>
2023-12-13 16:45:25 +0530

    Added Faker dependency for dynamic data

--- src/test/java/UserLoginTest.java
+++ src/test/java/UserLoginTest.java
@@ -1,26 +1,29 @@
 import clients.UserClient;
+import com.github.javafaker.Faker;
 import models.auth.LoginResponseBody;
 import models.auth.SignupResponseBody;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import testdata.UserData;
 import utilities.DataProvider;
-import utilities.RandomEmailGenerator;
 
 public class UserLoginTest extends BaseTest {
     private UserClient userClient;
     private DataProvider userDataProvider;
 
+    Faker faker;
+
     @BeforeClass
     public void beforeClass() {
         userClient = new UserClient();
         userDataProvider = new DataProvider("src/main/resources/testdata/userData.json");
+        faker = new Faker();
     }
 
     @Test
     public void loginSuccessfully() {
         // Arrange
-        String randomEmail = RandomEmailGenerator.generateRandomEmail();
+        String randomEmail = faker.internet().emailAddress();
         String password = userDataProvider.getData("validUser", UserData.class).getPassword();
 
         SignupResponseBody signupResponseBody = userClient.signup(randomEmail, password);
