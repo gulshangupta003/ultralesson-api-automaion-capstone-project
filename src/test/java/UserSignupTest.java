3380c96f5516b03dbf23a37fc6699b34873af7c0
gulshangupta003 <gulshangupta003@gmail.com>
2023-12-15 16:23:14 +0530

    Using generateRandomPassword method in signup test

--- src/test/java/UserSignupTest.java
+++ src/test/java/UserSignupTest.java
@@ -3,19 +3,16 @@ import jdk.jfr.Description;
 import models.auth.SignupResponseBody;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-import testdata.UserData;
 import utilities.DataProvider;
 import utilities.RandomDataUtils;
 
 public class UserSignupTest extends BaseTest {
     private UserClient userClient;
-    private DataProvider userDataProvider;
     private RandomDataUtils randomDataUtils;
 
     @BeforeClass
     public void beforeClass() {
         userClient = new UserClient();
-        userDataProvider = new DataProvider("src/main/resources/testdata/userData.json");
         randomDataUtils = new RandomDataUtils();
     }
 
@@ -25,7 +22,7 @@ public class UserSignupTest extends BaseTest {
         // Arrange
         String randomEmail = randomDataUtils.generateRandomEmail();
         assert isValidEmailFormat(randomEmail) : "Invalid email format: " + randomEmail;
-        String password = userDataProvider.getData("validUser", UserData.class).getPassword();
+        String password = randomDataUtils.generateRandomPassword();
 
         // Act
         SignupResponseBody signupResponseBodyBody = userClient.signup(randomEmail, password);
