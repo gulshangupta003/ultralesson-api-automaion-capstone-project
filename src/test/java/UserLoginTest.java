f57cf7ab65c290aa0d415518c5b3f2abee5c76f3
gulshangupta003 <gulshangupta003@gmail.com>
2023-12-18 14:33:18 +0530

    Changed test methods

--- src/test/java/UserLoginTest.java
+++ src/test/java/UserLoginTest.java
@@ -1,32 +1,21 @@
 import clients.UserClient;
 import jdk.jfr.Description;
 import models.auth.LoginResponseBody;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import testdata.UserData;
 import utilities.DataProvider;
 
 public class UserLoginTest extends BaseTest {
-    private UserClient userClient;
-    private DataProvider userDataProvider;
-
-    @BeforeClass
-    public void beforeClass() {
-        userClient = new UserClient();
-        userDataProvider = new DataProvider("src/main/resources/testdata/userData.json");
-    }
-
-
     @Test
     @Description("User should be able to login successfully with valid credentials")
     public void loginSuccessfully() {
         // Arrange
-        UserData validUser = userDataProvider.getData("validUser", UserData.class);
+        UserData validUser = new DataProvider("src/main/resources/testdata/userData.json").getData("validUser", UserData.class);
         String email = validUser.getEmail();
         String password = validUser.getPassword();
 
         // Act
-        LoginResponseBody loginResponseBody = userClient.login(email, password);
+        LoginResponseBody loginResponseBody = new UserClient().login(email, password);
 
         // Assert
         loginResponseBody.assertSuccessfullyLoginResponse(email);
--- src/test/java/UserSignupTest.java
+++ src/test/java/UserSignupTest.java
@@ -1,31 +1,21 @@
 import clients.UserClient;
 import jdk.jfr.Description;
 import models.auth.SignupResponseBody;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-import utilities.DataProvider;
 import utilities.RandomDataUtils;
 
 public class UserSignupTest extends BaseTest {
-    private UserClient userClient;
-    private RandomDataUtils randomDataUtils;
-
-    @BeforeClass
-    public void beforeClass() {
-        userClient = new UserClient();
-        randomDataUtils = new RandomDataUtils();
-    }
-
     @Test
     @Description("User should be able to signup successfully with valid credentials")
     public void successfullySignupUser() {
         // Arrange
+        RandomDataUtils randomDataUtils = new RandomDataUtils();
         String randomEmail = randomDataUtils.generateRandomEmail();
         assert isValidEmailFormat(randomEmail) : "Invalid email format: " + randomEmail;
         String password = randomDataUtils.generateRandomPassword();
 
         // Act
-        SignupResponseBody signupResponseBodyBody = userClient.signup(randomEmail, password);
+        SignupResponseBody signupResponseBodyBody = new UserClient().signup(randomEmail, password);
 
         // Assert
         signupResponseBodyBody.assertSuccessfullySignupResponse(randomEmail);
